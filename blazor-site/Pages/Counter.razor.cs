71775abe1ce595829afd97a0aa2b651bb804b3a0
robert-kuropkat <robert.kuropkat@gmail.com>
2024-11-06 19:49:37 -0500

    Separate code from Home.razor file

---
+++ blazor-site/Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+namespace blazor_site.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
